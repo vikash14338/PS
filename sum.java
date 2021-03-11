5eb02e4e0d14c5e0680ee544db05228888780b16
vikash kumar <vikash.kumar14338@gmail.com>
2021-03-11 09:07:46 +0530

    Sum feature added

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class SumNum{
+ public static void main(string[] args)
+	{
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(a+b);
+	}
+}
